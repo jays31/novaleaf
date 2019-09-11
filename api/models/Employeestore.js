526d12cf4649b35ae077bd3088f43d2412e1ef37
Jay <sharma.jay95@gmail.com>
2019-09-11 16:25:19 -0300

    Employee Management API fixed

--- api/models/Employeestore.js
+++ api/models/Employeestore.js
@@ -6,24 +6,34 @@
  */
 
 module.exports = {
-
   attributes: {
-
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
+    empName:{
+      type: 'string',
+      required: true,
+    },
+    empJoinDate:{
+      type: 'string',
+      required: true,
+    },
+    empPhone:{
+      type: 'number',
+      required: true,
+    },
+    empAddress:{
+      type: 'string',
+      required: true,
+    },
+    empType:{
+      type: 'string',
+      required: true,
+    },
+    empHourRate:{
+      type: 'number',
+      required: true,
+    },
+    empRole:{
+      type: 'string',
+      required: true,
+    },
   },
-
 };
-
